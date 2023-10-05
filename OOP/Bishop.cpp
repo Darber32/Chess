2b3a766ea3b965cd54978209963edc02eb48b175
Darber32 <sergey_sbrodov@list.ru>
2023-10-05 19:31:43 +0300

    Added Bishop movement

--- OOP/Bishop.cpp
+++ OOP/Bishop.cpp
@@ -10,7 +10,121 @@ Bishop::Bishop(Color color, int x, int y, sf::Image image)
     Set_Position(x, y);
     Set_Sprite(image, 1);
 }
-void Bishop::Show_Avalible_Positions()
+void Bishop::Show_Avalible_Positions(Table& table, int& c, Marker** markers_mass)
 {
-    return;
+    for (int i = 0; i < c; i++)
+        markers_mass[i]->Clear(markers_mass[i]);
+    c = 0;
+    int i = 0, offset = size;
+    bool flag = true;
+    while (flag)
+    {
+        if (y - offset <= -size)
+            flag = false;
+        if (flag)
+        {
+            if (x - offset <= -size)
+                flag = false;
+            if (table.Get_Position((y - offset) / size, (x - offset) / size) == nullptr)
+            {
+                markers_mass[i]->Set_Position(x - offset, y - offset);
+                i++;
+                c++;
+                offset += size;
+            }
+            else if (table.Get_Position((y - offset) / size, (x - offset) / size)->Get_Color() != color)
+            {
+                markers_mass[i]->Set_Position(x - offset, y - offset);
+                i++;
+                c++;
+                flag = false;
+            }
+            else
+                flag = false;
+        }
+    }
+    offset = size;
+    flag = true;
+    while (flag)
+    {
+        if (y - offset <= -size)
+            flag = false;
+        if (flag)
+        {
+            if (x + offset >= 800)
+                flag = false;
+            if (table.Get_Position((y - offset) / size, (x + offset) / size) == nullptr)
+            {
+                markers_mass[i]->Set_Position(x + offset, y - offset);
+                i++;
+                c++;
+                offset += size;
+            }
+            else if (table.Get_Position((y - offset) / size, (x + offset) / size)->Get_Color() != color)
+            {
+                markers_mass[i]->Set_Position(x + offset, y - offset);
+                i++;
+                c++;
+                flag = false;
+            }
+            else
+                flag = false;
+        }
+    }
+    offset = size;
+    flag = true;
+    while (flag)
+    {
+        if (y + offset >= 800)
+            flag = false;
+        if (flag)
+        {
+            if (x + offset >= 800)
+                flag = false;
+            if (table.Get_Position((y + offset) / size, (x + offset) / size) == nullptr)
+            {
+                markers_mass[i]->Set_Position(x + offset, y + offset);
+                i++;
+                c++;
+                offset += size;
+            }
+            else if (table.Get_Position((y + offset) / size, (x + offset) / size)->Get_Color() != color)
+            {
+                markers_mass[i]->Set_Position(x + offset, y + offset);
+                i++;
+                c++;
+                flag = false;
+            }
+            else
+                flag = false;
+        }
+    }
+    offset = size;
+    flag = true;
+    while (flag)
+    {
+        if (y + offset >= 800)
+            flag = false;
+        if (flag)
+        {
+            if (x - offset <= -size)
+                flag = false;
+            if (table.Get_Position((y + offset) / size, (x - offset) / size) == nullptr)
+            {
+                markers_mass[i]->Set_Position(x - offset, y + offset);
+                i++;
+                c++;
+                offset += size;
+            }
+            else if (table.Get_Position((y + offset) / size, (x - offset) / size)->Get_Color() != color)
+            {
+                markers_mass[i]->Set_Position(x - offset, y + offset);
+                i++;
+                c++;
+                flag = false;
+            }
+            else
+                flag = false;
+        }
+    }
 }
