c59cc743111d517c8612df326200c911c6971922
Darber32 <sergey_sbrodov@list.ru>
2023-10-05 16:49:20 +0300

    Added Castle movement

--- OOP/Castle.h
+++ OOP/Castle.h
@@ -5,5 +5,5 @@ class Castle : public Figure
 {
 public:
     Castle(Color color, int x, int y, sf::Image image);
-    void Show_Avalible_Positions();
+    void Show_Avalible_Positions(Table& table, int& c, Marker** markers_mass);
 };
